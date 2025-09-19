d4b347233185b7aa7162ab6bb557f8e07f00d8f5
Juan Carlos Pérez González <jcarlos@teis.intranet>
2025-09-19 09:49:11 +0200

    preambulo chkdni

--- Customers.py
+++ Customers.py
@@ -5,5 +5,28 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 class Customers:
     @staticmethod
     def checkDni(self=None):
-        dni = globals.ui.txtDnicli.text()
-        print(dni)
+        try:
+            dni = globals.ui.txtDnicli.text()
+            dni = str(dni).upper()
+            tabla = "TRWAGMYFPDXBNJZSQVHLCKE"
+            dig_ext = "XYZ"
+            reemp_dig_ext = {'X': '0', 'Y': '1', 'Z': '2'}
+            numeros = "1234567890"
+            if len(dni) == 9:
+                dig_control = dni[8]
+                dni = dni[:8]
+                if dni[0] in dig_ext:
+                    dni = dni.replace(dni[0], reemp_dig_ext[dni[0]])
+                if len(dni) == len([n for n in dni if n in numeros]) and tabla[int(dni) % 23] == dig_control:
+                    var.ui.txtDnicli.setStyleSheet('background-color: rgb(255, 255, 220);')
+                else:
+                    var.ui.txtDnicli.setStyleSheet('background-color:#FFC0CB;')
+                    var.ui.txtDnicli.setText(None)
+                    var.ui.txtDnicli.setFocus()
+            else:
+                var.ui.txtDnicli.setStyleSheet('background-color:#FFC0CB;')
+                var.ui.txtDnicli.setText(None)
+                var.ui.txtDnicli.setFocus()
+        except Exception as error:
+            print("error en validar dni ", error)
+
